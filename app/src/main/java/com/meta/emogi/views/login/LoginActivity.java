67dadbed43ad6e31c9dbb27e2de4ba91a0e3c5e6
1yechan <1yechan@mirusystems.com>
2025-01-19 19:21:07 +0800

    feat : play store 버전확인 기능 추가
    play store 버전확인 기능 추가

--- app/src/main/java/com/meta/emogi/views/login/LoginActivity.java
+++ app/src/main/java/com/meta/emogi/views/login/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
 import com.meta.emogi.MyApplication;
 import com.meta.emogi.R;
 import com.meta.emogi.base.BaseActivity;
@@ -20,6 +25,47 @@ import com.meta.emogi.views.splash.SplashActivity;
 import com.meta.emogi.views.toolbar.ToolbarView;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding> {
+
+    private static final int UPDATE_REQUEST_CODE = 1234;
+    private AppUpdateManager appUpdateManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        // AppUpdateManager 초기화
+        appUpdateManager = AppUpdateManagerFactory.create(this);
+
+        // 업데이트 상태 확인
+        appUpdateManager.getAppUpdateInfo().addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+
+                // 즉시 업데이트 요청
+                try {
+                    appUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo,
+                            AppUpdateType.IMMEDIATE,
+                            this,
+                            UPDATE_REQUEST_CODE);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == UPDATE_REQUEST_CODE && resultCode != RESULT_OK) {
+            // 업데이트 실패 시 처리
+            Toast.makeText(this, "업데이트가 필요합니다.", Toast.LENGTH_SHORT).show();
+            finishAffinity();
+        }
+    }
+
 
     private static final String TAG = "LoginActivity";
 
