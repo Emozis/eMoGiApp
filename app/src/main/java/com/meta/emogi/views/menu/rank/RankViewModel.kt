334915f728c8723da51a2eba232160185647279c
1yechan <wy7wy7@naver.com>
2025-10-25 15:03:13 +0900

    feat: 메뉴(홈,랭킹) 더미 UI
    - 피그마 기반 UI 작업 (메뉴)

--- app/src/main/java/com/meta/emogi/views/menu/rank/RankViewModel.kt
+++ app/src/main/java/com/meta/emogi/views/menu/rank/RankViewModel.kt
@@ -2,6 +2,7 @@ package com.meta.emogi.views.menu.rank
 
 import android.view.View
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.meta.emogi.R
 import com.meta.emogi.base.BaseViewModel
 import com.meta.emogi.base.SingleLiveEvent
@@ -9,11 +10,17 @@ import com.meta.emogi.base.SingleLiveEvent
 class RankViewModel : BaseViewModel() {
 
     private val _Go2HomeFrag = SingleLiveEvent<Unit>()
+    private val _selectedRankType = MutableLiveData(1)
 
     fun Go2HomeFrag() : LiveData<Unit>{
         return _Go2HomeFrag
     }
 
+    val selectedRankType: LiveData<Int> = _selectedRankType
+
+    fun selectRankType(type:Int){
+        _selectedRankType.value = type
+    }
 
     override fun onButtonClicked(v: View): Boolean {
         if(!super.onButtonClicked(v)) return false
